589e5529cd0289818e7cfe1e83d88d00dbad3471
Joe J. Howard <joe.howard.au@icloud.com>
2022-09-30 15:30:50 +1000

    Error Logging Test

---
+++ tests/unit/exception/ErrorLoggerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @copyright Joe J. Howard
+ * @license   https://github.com/Serve-Framework/Framework/blob/master/LICENSE
+ */
+
+namespace serve\tests\unit\framework\exception;
+
+use ErrorException;
+use serve\exception\logger\Logger;
+use serve\file\Filesystem;
+use serve\http\request\Environment;
+use serve\tests\TestCase;
+use function dirname;
+
+/**
+ * @group unit
+ */
+class ErrorLoggerTest extends TestCase
+{
+    /**
+     *
+     */
+    protected function getServerData(): array
+    {
+        return
+        [
+            'REQUEST_METHOD'  => 'GET',
+            'SCRIPT_NAME'     => '/foobar/index.php',
+            'SERVER_NAME'     => 'localhost',
+            'SERVER_PORT'     => '8888',
+            'HTTP_PROTOCOL'   => 'http',
+            'DOCUMENT_ROOT'   => '/usr/name/httpdocs',
+            'HTTP_HOST'       => 'http://localhost:8888',
+            'DOMAIN_NAME'     => 'localhost:8888',
+            'REQUEST_URI'     => '/foobar?foo=bar',
+            'REMOTE_ADDR'     => '192.168.1.1',
+            'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17',
+        ];
+    }
+
+    /**
+     *
+     */
+    public function testWebHandler(): void
+    {
+        $this->expectNotToPerformAssertions();
+
+        $path        = dirname(__FILE__) . '/testlogs';
+        $fileSystem  = $this->mock(Filesystem::class);
+        $environment = new Environment($this->getServerData());
+        $logger      = new Logger($fileSystem, $environment, $path);
+
+        $fileSystem->shouldReceive('appendContents');
+
+        $logger->writeException(new ErrorException('Something bad happened'));
+    }
+}
